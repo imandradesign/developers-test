80ed2ced0b80da0fc5658e6385f1b1ad178bdb8e
Imandra McKenzie <indigoimandra@gmail.com>
2019-04-10 13:30:23 -0700

    Update JS to properly push skill checkboxes to the array and update post formatting

--- js/scripts.js
+++ js/scripts.js
@@ -11,62 +11,71 @@ function JobPosting(companyName, title, difficulty, location, description) {
 
 JobPosting.prototype.entryPost = function(){
   return '<div class="alert alert-success">' +
-    '<h5 class="alert-heading"><strong>' +
+    '<h5 class="alert-heading"><strong><center>' +
     this.title +
-    '</strong></h5>' +
-    '<p>Company name: ' +
+    '</center></strong></h5>' +
+    '<p class="label">Company name: </p>' +
+    '<p>' +
     this.companyName +
     '</p>' +
-    '<p>Location: ' +
+    '<p class="label">Location: </p>' +
+    '<p>' +
     this.location +
     '</p>' +
-    '<ul>' +
-    '<span id="bg-skills"></span>' +
-    '</ul>' +
-    '<p>Description: ' +
-    this.description +
+    '<p class="label">Required Skills: </p>' +
+    '<p>' +
+    this.skills +
     '</p>' +
-    '</div>'
+    '<p class="label">Description: </p>' +
+    '<p>' +
+    this.description +
+    '</p></div>'
 }
 
 JobPosting.prototype.intPost = function(){
   return '<div class="alert alert-info">' +
-    '<h5 class="alert-heading"><strong>' +
-    this.title +
-    '</strong></h5>' +
-    '<p>Company name: ' +
-    this.companyName +
-    '</p>' +
-    '<p>Location: ' +
-    this.location +
-    '</p>' +
-    '<ul>' +
-    '<span id="bg-skills"></span>' +
-    '</ul>' +
-    '<p>Description: ' +
-    this.description +
-    '</p>' +
-    '</div>'
+  '<h5 class="alert-heading"><strong><center>' +
+  this.title +
+  '</center></strong></h5>' +
+  '<p class="label">Company name: </p>' +
+  '<p>' +
+  this.companyName +
+  '</p>' +
+  '<p class="label">Location: </p>' +
+  '<p>' +
+  this.location +
+  '</p>' +
+  '<p class="label">Required Skills: </p>' +
+  '<p>' +
+  this.skills +
+  '</p>' +
+  '<p class="label">Description: </p>' +
+  '<p>' +
+  this.description +
+  '</p></div>'
 }
 
 JobPosting.prototype.advPost = function(){
   return '<div class="alert alert-primary">' +
-    '<h5 class="alert-heading"><strong>' +
-    this.title +
-    '</strong></h5>' +
-    '<p>Company name: ' +
-    this.companyName +
-    '</p>' +
-    '<p>Location: ' +
-    this.location +
-    '</p>' +
-    '<ul>' +
-    '<span id="bg-skills"></span>' +
-    '</ul>' +
-    '<p>Description: ' +
-    this.description +
-    '</p>' +
-    '</div>'
+  '<h5 class="alert-heading"><strong><center>' +
+  this.title +
+  '</center></strong></h5>' +
+  '<p class="label">Company name: </p>' +
+  '<p>' +
+  this.companyName +
+  '</p>' +
+  '<p class="label">Location: </p>' +
+  '<p>' +
+  this.location +
+  '</p>' +
+  '<p class="label">Required Skills: </p>' +
+  '<p>' +
+  this.skills +
+  '</p>' +
+  '<p class="label">Description: </p>' +
+  '<p>' +
+  this.description +
+  '</p></div>'
 }
 
 
@@ -91,26 +100,21 @@ $(document).ready(function(){
     var difficulty = $("#difficulty").val();
     var jobLocation = $("#location").val();
     var jobDescription = $("#description").val();
-    var jobSkills = $("input:checkbox[name=skills]:checked").map(function(){
-      newPost.skills.push($(this).val());
-      return newPost.skills;
-    });
 
     var newPost = new JobPosting(companyName, jobTitle, difficulty, jobLocation, jobDescription);
-    newPost.entryPost();
+
+    $("input:checkbox[name=skills]:checked").map(function(){
+      newPost.skills.push(" " + $(this).val());
+      return newPost.skills;
+    });
 
     if (difficulty === "entry"){
        $("#entry-level").append(newPost.entryPost());
 
-       jobSkills.each(function(){
-         $("#bg-skills").text("<li>" + this.skills + "</li>");
-       })
      } else if (difficulty === "intermediate"){
        $("#int-level").append(newPost.intPost());
-
      } else if (difficulty === "advanced"){
        $("#adv-level").append(newPost.advPost());
-
     }
     $(".post-form").hide();
     $("#jobpost-form")[0].reset();
